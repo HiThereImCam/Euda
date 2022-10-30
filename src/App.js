ed4d22062ccf643ef9f03d783b598d3f0a079512
Cameron Tanjoco <ctanjoco@gmail.com>
2022-10-30 00:26:29 -0700

    adding garden background

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React from 'react';
 import './App.css';
 import Modal from './Components/Modal';
-/*
-  Each modal has internal state? 
-  Or should there be an overarching Modal that controls which modal to open
-    - i think we pass state down to the components
-
-  Modal
-    -
-
-      switch(){
-        case calendar:
-          return <Calendar />
-        case resources:
-          return <Resources />
-        case connect: 
-          return <Connect />
-
-      }
-
-      onClick = () => setState()
-
-      - <Calendar open={}/> 
-          - if Calendar State 
-            - render Modal
-
-
-
-*/
+import Garden from "./images/garden.svg"
 
 function App() {
   return (
     <div className="App">
-      <Modal />
+      <img src={Garden} alt="garden background" />
+      {/* <Modal /> */}
     </div>
   );
 }
