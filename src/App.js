128060b5c7b075bbe9f45dd8f37d51c475450fcc
Cameron Tanjoco <ctanjoco@gmail.com>
2022-10-30 10:29:58 -0700

    adding updated changes to modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Modal from './Components/garden_modals/Modal';
+import MoodModal from './Components/sky_modals/moodModal';
 
 function App() {
+  const [moodState, setMoodState] = useState(false)
   return (
     <div className="App">
       <section className="sky">
-        
+        <MoodModal setMoodState={setMoodState}/>
+         {/* need to add gratitude modal  */}
       </section>
       <section className="garden">
         <Modal />
