ebfd3250efc18ffeafb1fd53a1ddd16b587df9f5
Cameron Tanjoco <ctanjoco@gmail.com>
2022-10-30 12:06:53 -0700

    mood_modal finished

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import './App.css';
 import Modal from './Components/garden_modals/Modal';
 import MoodModal from './Components/sky_modals/moodModal';
-
+import Grateful from './Components/sky_modals/Grateful';
 
 
 function App() {
   const [moodState, setMoodState] = useState(false)
-  const [gratefulState, setGratefulState] = useState(flase)
+  const [gratefulState, setGratefulState] = useState(false)
 
 const moveBrowserDown = () => {
     // movebrowserdown
@@ -19,7 +19,8 @@ const moveBrowserDown = () => {
          {/* need to add gratitude modal  */}
         {/* <img src={CloudsBackground} alt="calendar" className='mood-img'/> */}
         <MoodModal setMoodState={setMoodState}/>
-        <Grateful />
+        {moodState &&  <Grateful /> }
+       
         {moodState && gratefulState && moveBrowserDown}
       </section>
       <section className="garden">
--- src/Components/sky_modals/moodModal.js
+++ src/Components/sky_modals/moodModal.js
@@ -1,4 +1,9 @@
-import Grateful from "./Grateful";
+import lvl1 from "../../images/moods/moodlevel1.svg"
+import lvl2 from "../../images/moods/moodlevel2.svg"
+import lvl3 from "../../images/moods/moodlevel3.svg"
+import lvl4 from "../../images/moods/moodlevel4.svg"
+import lvl5 from "../../images/moods/moodlevel5.svg"
+
 const MoodModal = ({setMoodState}) => {
     /**
      * I imagine transitions
@@ -9,12 +14,18 @@ const MoodModal = ({setMoodState}) => {
     
     return(
      <div className="mood_modal-container">
+        <div className="header-container">
+        <p className="welcome-header">Welcome Back!</p>
+        <p className="question">How are we feeling today?</p>
+        </div>
 
-        <h4>How are you feeling today?</h4>
-
-        {/* <h4>How are you feeling today?</h4> */}
-
-
+        <div className="mood-container" onClick={() => setMoodState(true)}>
+            <img src={lvl1} alt="very sad"/>
+            <img src={lvl2} alt="somewhat sad"/>
+            <img src={lvl3} alt="neutral"/>
+            <img src={lvl4} alt="somewhat happy"/>
+            <img src={lvl5} alt="very happy"/>
+        </div>
      </div>   
     )
 }
